e319795182c3f59afbe698f73bdecd6311be6324
stubborngastropod <dnchudnjee@gmail.com>
2023-01-21 23:54:24 +0900

    unsolved

--- boj_1260.py
+++ boj_1260.py
@@ -9,16 +9,29 @@ for i in range(M):
     a, b = map(int, input().split())
     adj_matrix[a][b], adj_matrix[b][a] = 1, 1
 
-visited = []
+dfs_visited = []
+bfs_visited = []
 
 def dfs(graph, root):
-    visited.append(root)
+    dfs_visited.append(root)
     print(root, end = ' ')
     for i in range(1, N + 1):
-        if graph[root][i] == 1 and i not in visited:
+        if graph[root][i] == 1 and i not in dsf_visited:
             newroot = i
             dfs(graph, newroot)
         else:
             None
+
+def bfs(graph, root):
+    queue = []
+    bfs_visited.append(root)
+    print(root, end=' ')
+    for i in range(1, N + 1):
+        if graph[root][i] == 1 and i not in bfs_visited:
+            queue.append(i)
+        else:
+            None
+        bfs_visited.pop()
+        newroot = bfs_visited.pop()
 
 dfs(adj_matrix, V)
---
+++ boj_18111.py
@@ -0,0 +1,31 @@
+import sys
+
+N, M, B = map(int, sys.stdin.readline().split())
+land = []
+
+for i in range(N):
+    block = list(map(int, sys.stdin.readline().split()))
+    land = land + block
+
+max_height = max(land)
+min_height = min(land)
+shortest_time = 64000000
+
+for i in range(min_height, max_height + 1):
+    spendtime = 0
+    spendblock = 0
+    for j in land:
+        if j <= i:
+            spendtime += (i - j)
+            spendblock += i - j
+        else:
+            spendtime += (j - i) * 2
+
+    if spendblock <= B and spendtime <= shortest_time:
+        shortest_time = spendtime
+        final_height = i
+
+print(f'{shortest_time} {final_height}')
+
+
+
